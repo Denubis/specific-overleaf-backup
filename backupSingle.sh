a3e297e3937e90c1945dbc6fcea4acde9fac6eca
Brian Ballsun-Stanton <brian@fedarch.org>
2020-01-24 02:12:51 +0000

    I hope this works

--- backupSingle.sh
+++ backupSingle.sh
@@ -1,4 +1,25 @@
-for line in `cat papersToBackup.txt`
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -p data
+
+for line in $(cat papersToBackup.txt)
 do
-echo $line
+repo=$( echo "$line" | cut -d'/' -f4 -)
+if [ ! -d "data/$repo/.git" ]; then
+	git -C data clone "$line"
+	echo "Please enter the github url of this repository"
+	read githuburl
+	cd "data/$repo"
+	git remote add github "$githuburl"
+	git pull github master
+	git push github master
+else
+	echo "Repo ${repo} exists, updating"
+	cd "data/$repo"
+	git pull github master
+	git pull
+	git push github master
+fi
 done
